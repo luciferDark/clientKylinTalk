1e112c44b96e81a73fce12c765768ae194687ca0
kylin <342748272@qq.com>
2019-07-26 00:32:21 +0800

    添加Common 库中基础Activity和Fragment类

---
+++ common/src/main/java/com/ll/common/app/Activity.java
@@ -0,0 +1,96 @@
+package com.ll.common.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+/**
+ * @author kylin
+ */
+public abstract class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initWindows();
+
+        if (initArgs(getIntent().getExtras())) {
+            int _layoutId = getContentLayoutId();
+            setContentView(_layoutId);
+            initWidget();
+            initData();
+        } else {
+            finish();
+        }
+
+    }
+
+    /**
+     * 初始化窗口设置
+     */
+    protected void initWindows() {
+
+    }
+
+    /**
+     * 初始化参数是否正确
+     * @param bundle
+     * @return
+     */
+    protected boolean initArgs(Bundle bundle) {
+        return true;
+    }
+
+    /**
+     * 获取布局文件id接口
+     *
+     * @return 布局文件id
+     */
+    protected abstract int getContentLayoutId();
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget() {
+
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        //当点击导航返回时，结束当前界面
+        finish();
+        return super.onSupportNavigateUp();
+    }
+
+    /**
+     *  返回点击
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        List<android.support.v4.app.Fragment> fragments = getSupportFragmentManager().getFragments();
+
+        if (fragments != null && fragments.size() > 0){
+            for (Fragment fragment : fragments) {
+                if (fragment instanceof com.ll.common.app.Fragment){
+                    if (((com.ll.common.app.Fragment)fragment).onBackPressed()){
+                        //判断fragment中是否拦截了返回键事件，是则直接返回
+                        return;
+                    }
+                }
+            }
+        }
+
+        finish();
+    }
+}
---
+++ common/src/main/java/com/ll/common/app/Fragment.java
@@ -0,0 +1,87 @@
+package com.ll.common.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class Fragment extends android.support.v4.app.Fragment {
+    protected  View mRootView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        initArgs(getArguments());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mRootView == null){
+            int _layoutId = getContentLayoutId();
+            View view = inflater.inflate(_layoutId,container, false);
+            mRootView = view;
+            initWidget(mRootView);
+        } else {
+            if (mRootView.getParent()!= null){
+                // 移除其父控件
+                ((ViewGroup)(mRootView.getParent())).removeView(mRootView);
+            }
+        }
+
+        return mRootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Fragment 创建完成后初始化数据
+        initData();
+    }
+
+    /**
+     * 获取布局文件id接口
+     *
+     * @return 布局文件id
+     */
+    protected abstract int getContentLayoutId();
+
+    /**
+     * 初始化参数是否正确
+     * @param bundle
+     */
+    protected void initArgs(Bundle bundle) {
+    }
+
+    /**
+     * 初始化窗口设置
+     */
+    protected void initWindows() {
+
+    }
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget(View view) {
+
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+    /**
+     * 处理返回键逻辑
+     * @return true 标识拦截返回键处理，false 不拦截
+     */
+    public boolean onBackPressed(){
+        return  false;
+    }
+}
