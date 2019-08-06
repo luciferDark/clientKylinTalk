8304a5a241cbbc0dc8b3f4574d6827c05c521e06
kylin <342748272@qq.com>
2019-08-06 19:13:41 +0800

    1：完善封装RecyclerView适配器回调

--- common/src/main/java/com/ll/common/widget/recycler/AdapterCallback.java
+++ common/src/main/java/com/ll/common/widget/recycler/AdapterCallback.java
@@ -4,6 +4,6 @@ package com.ll.common.widget.recycler;
  * @author  kylin
  * RecyclerView 回调
  */
-public interface AdapterCallback {
-
+public interface AdapterCallback<Data> {
+    void update(Data data, RecyclerAdapter.ViewHolder<Data> holder);
 }
--- common/src/main/java/com/ll/common/widget/recycler/RecyclerAdapter.java
+++ common/src/main/java/com/ll/common/widget/recycler/RecyclerAdapter.java
@@ -1,29 +1,112 @@
 package com.ll.common.widget.recycler;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ll.common.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
- * @author  Kylin
+ * @author Kylin
  * 封装RecyclerAdpater
  */
-public class RecyclerAdapter extends RecyclerView.Adapter {
-
+public abstract class RecyclerAdapter<Data> extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder<Data>>
+        implements View.OnClickListener, View.OnLongClickListener {
+    private final List<Data> mDataList = new ArrayList<>();//View对应的数据集合
+    /**
+     * 创建一个ViewHolder
+     *
+     * @param parent   RecyclerView
+     * @param viewType 约定好viewType是用的Xml的id
+     * @return ViewHolder
+     */
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder<Data> onCreateViewHolder(@NonNull ViewGroup parent, @LayoutRes int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View root = inflater.inflate(viewType, parent, false);
+        ViewHolder<Data> holder = createViewHolder(root, viewType);
+
+        root.setOnClickListener(this);
+        root.setOnLongClickListener(this);
+        // 设置Tag绑定holder
+        root.setTag(R.id.tag_recycler_adapter_holder, holder);
+
+        //butterKnife 绑定
+        holder.mUnbinder = ButterKnife.bind(holder, root);
+
+        return holder;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    protected abstract ViewHolder<Data> createViewHolder(View root, int viewType);
 
+    /**
+     * 绑定数据到ViewHolder上
+     *
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder<Data> holder, int position) {
+        if (mDataList == null || mDataList.size() <= 0) {
+            return;
+        }
+        Data data = mDataList.get(position);
+        holder.bindData(data);//绑定界面数据
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataList == null ? 0 : mDataList.size();
+    }
+
+
+    public static abstract class ViewHolder<Data> extends RecyclerView.ViewHolder {
+        protected Data mData;
+        private Unbinder mUnbinder;
+
+        private AdapterCallback<Data> adapterCallback;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        void bindData(Data data) {
+            this.mData = data;
+            onBind(data);
+        }
+
+        public void setAdapterCallback(AdapterCallback<Data> adapterCallback) {
+            this.adapterCallback = adapterCallback;
+        }
+
+        /**
+         * 数据绑定接口
+         * 当绑定数据时会触发回调，需要复写接口
+         *
+         * @param data
+         */
+        protected abstract void onBind(Data data);
+
+        /**
+         * 更新数据
+         *
+         * @param data
+         */
+        public void updateData(Data data) {
+            if (this.adapterCallback != null){
+                this.adapterCallback.update(data, this);
+            }
+        }
     }
 }
